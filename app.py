1564f9c273ac1121668922e2098d42a29e90f54a
Muhammad Abdullah <154841585+muhammadabdullah12345@users.noreply.github.com>
2026-02-20 17:36:21 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pickle
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
